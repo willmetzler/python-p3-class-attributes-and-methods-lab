2c5e309de96113b30e37a74a403bee19bddade95
Will Metzler <metzlerwill@gmail.com>
2024-04-24 15:17:02 -0400

    pass tests

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,23 @@
 class Song:
-    pass
+
+    all_songs = []
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.all_songs.append(self)
+        
+        Song.count += 1
+        if genre not in Song.genres:
+            Song.genres.append(genre)
+        if artist not in Song.artists:
+            Song.artists.append(artist)
+
+        Song.genre_count[genre] = Song.genre_count.get(genre, 0) + 1
+        Song.artist_count[artist] = Song.artist_count.get(artist, 0) + 1
